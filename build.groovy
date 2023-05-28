3286d86a6af5ba9a6d3b6f1d227e05b0ffe8049c
Bommasani Vamsi Krishna <bommasani@Bommasanis-MBP.lan>
2023-05-28 17:14:46 -0500

    added jfrog

--- build.groovy
+++ build.groovy
@@ -5,38 +5,49 @@ options {
     timestamps()
     }
   stages {
-    stage('CheckOut') {
+    stage('SCM') {
       steps {
+        cleanWs()
         echo 'Checking out project from Bitbucket....'
-          git branch: 'main', url: 'git@github.com:vamsi8977/ant_sample.git'
+        git branch: 'main', url: 'git@github.com:vamsi8977/ant_sample.git'
       }
     }
-stage('build') {
+    stage('Build') {
       steps {
         ansiColor('xterm') {
           echo 'Ant Build....'
-           sh "ant -buildfile build.xml"
+          sh "ant -buildfile build.xml"
         }
       }
     }
-stage('SonarQube') {
-    steps {
+    stage('SonarQube') {
+      steps {
         withSonarQubeEnv('SonarQube') {
-            sh "sonar-scanner"
+          sh "sonar-scanner"
         }
+      }
     }
-}
+    stage('JFrog') {
+      steps {
+        ansiColor('xterm') {
+          sh '''
+            jf rt u build/jar/*.jar ant/
+            jf scan build/jar/*.jar --fail-no-op --build-name=gradle --build-number=$BUILD_NUMBER
+          '''
+        }
+      }
+    }
   }//end stages
-post {
-      success {
-          archiveArtifacts artifacts: "build/jar/*.jar"
-          echo "The build passed."
-      }
-      failure {
-          echo "The build failed."
-      }
-      cleanup{
-        deleteDir()
-      }
+  post {
+    success {
+      archiveArtifacts artifacts: "build/jar/*.jar"
+      echo "The build passed."
     }
+    failure {
+      echo "The build failed."
+    }
+    cleanup {
+      deleteDir()
+    }
+  }
 }
